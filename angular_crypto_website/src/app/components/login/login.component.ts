4586a85beb3d21b07e09ae0e982e46c1fb26de6b
argun8394 <argundoganali@gmail.com>
2023-01-09 16:57:51 +0300

    loginComponent

--- angular_crypto_website/src/app/components/login/login.component.ts
+++ angular_crypto_website/src/app/components/login/login.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  token: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
+    // console.log(this.loginForm)
   }
 
+  createForm() {
+    this.loginForm = this.fb.group({
+      identifier: ['test@test.com', [Validators.required]],
+      password: ['123abc', [Validators.required]],
+    });
+  }
+
+  get formValue() {
+    return this.loginForm.value;
+  }
+
+  onSubmit() {
+    const { identifier, password } = this.formValue;
+    this.auth.loginUser(identifier, password).subscribe(
+      (res) => {
+        localStorage.setItem('token', res.token);
+        // this.router.navigate(['/home'])
+        console.log(res.token);
+        this.token = localStorage.getItem('token');
+      },
+      (err) => console.log(err)
+    );
+  }
 }
---
+++ angular_crypto_website/src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/User.model';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public userSubject: BehaviorSubject<User>;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.userSubject = new BehaviorSubject<User>(null);
+  }
+
+  loginUser(identifier: string, password: string) {
+    const url: string = `${environment.apiurl}auth/login`;
+    const user: any = { identifier: identifier, password: password };
+
+    return this.http.post<any>(url, user)
+  }
+
+  logoutUser() {
+    localStorage.removeItem('token')
+    this.router.navigate(['/login'])
+  }
+
+  getToken() {
+    return localStorage.getItem('token')
+  }
+
+  loggedIn() {
+    return !!localStorage.getItem('token')
+  }
+}
